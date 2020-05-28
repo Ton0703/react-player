22e1ffda9799f136eb590e5906420887bf3a60d1
Ton0703 <44728377+Ton0703@users.noreply.github.com>
2020-05-29 00:52:40 +0800

    Updates

---
+++ precache-manifest.ec62066ddb8a27f8e38407b7567f7c76.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df16833c6ce49e1e316a5a9d5093fb53",
+    "url": "/react-player/index.html"
+  },
+  {
+    "revision": "0305de25058ebd6097c8",
+    "url": "/react-player/static/css/2.d34346ea.chunk.css"
+  },
+  {
+    "revision": "a9dcea445b5e4b25759d",
+    "url": "/react-player/static/css/main.4bcb77d2.chunk.css"
+  },
+  {
+    "revision": "8e457e3b62405339a9ec",
+    "url": "/react-player/static/js/10.f45f5526.chunk.js"
+  },
+  {
+    "revision": "f3b3c81eec9a1074c92a",
+    "url": "/react-player/static/js/11.7a0b16d9.chunk.js"
+  },
+  {
+    "revision": "ddc0e53d35f2ea1066c9",
+    "url": "/react-player/static/js/12.85e5d894.chunk.js"
+  },
+  {
+    "revision": "f80343b80412131ea57e",
+    "url": "/react-player/static/js/13.4ceeb891.chunk.js"
+  },
+  {
+    "revision": "6265c242b1efc24a4be7",
+    "url": "/react-player/static/js/14.7376e8e4.chunk.js"
+  },
+  {
+    "revision": "0305de25058ebd6097c8",
+    "url": "/react-player/static/js/2.ebbc69f9.chunk.js"
+  },
+  {
+    "revision": "ac708def751a8292e55daf5717e57737",
+    "url": "/react-player/static/js/2.ebbc69f9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1f32cbcaf09c5495a803",
+    "url": "/react-player/static/js/3.90de0119.chunk.js"
+  },
+  {
+    "revision": "917b98e95c99dddb6544",
+    "url": "/react-player/static/js/4.7702fea5.chunk.js"
+  },
+  {
+    "revision": "92e592f5f8e01930f99d",
+    "url": "/react-player/static/js/5.c02024f7.chunk.js"
+  },
+  {
+    "revision": "486575424482e49a088b",
+    "url": "/react-player/static/js/6.6bf71fdb.chunk.js"
+  },
+  {
+    "revision": "e33ef73add976dcc706f",
+    "url": "/react-player/static/js/7.29a5e22f.chunk.js"
+  },
+  {
+    "revision": "83fce35877bc18e12694",
+    "url": "/react-player/static/js/8.71f61b98.chunk.js"
+  },
+  {
+    "revision": "ead26cac0177572047c4",
+    "url": "/react-player/static/js/9.11089bf4.chunk.js"
+  },
+  {
+    "revision": "a9dcea445b5e4b25759d",
+    "url": "/react-player/static/js/main.3aff3612.chunk.js"
+  },
+  {
+    "revision": "0a85c46c55205c8ab866",
+    "url": "/react-player/static/js/runtime-main.3bf5cb20.js"
+  }
+]);
